74a846b6ab2ab41e46a88d0955361f5658078db8
yarbroughrh <yarbroughrh@gmail.com>
2022-04-01 15:44:43 -0500

    Initial Commit

---
+++ AquaponicMonitor/AquaponicMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaponicMonitorApp.swift
+//  AquaponicMonitor
+//
+//  Created by Robert Yarbrough on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AquaponicMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
