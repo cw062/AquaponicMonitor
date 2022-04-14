43dfdae2b24126360a3aa401313114fba492ab5c
Calder West <cw062@uark.edu>
2022-04-14 16:30:58 -0500

    added Signin

--- AquaponicMonitor/AquaponicMonitorApp.swift
+++ AquaponicMonitor/AquaponicMonitorApp.swift
@@ -11,15 +11,26 @@ import Firebase
 @main
 struct AquaponicMonitorApp: App {
     
-    init() {
-        FirebaseApp.configure()
-    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+      init() {
+           FirebaseApp.configure()
+
     }
-    
-    
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+
+        var body: some Scene {
+            WindowGroup {
+                let viewModel = AppViewModel()
+                SignIn()
+                    .environmentObject(viewModel)
+            }
         }
     }
-}
+
+    class AppDelegate: NSObject, UIApplicationDelegate{
+        func applicationDidFinishLaunching(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+                                           [UIApplication.LaunchOptionsKey : Any]? = nil)->Bool {
+            FirebaseApp.configure()
+
+            return true
+        }
+    }
